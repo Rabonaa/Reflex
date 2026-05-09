d0e0ccdd1c7c0666bae1df21b5a339c37e505cfb
Rabonaa <rabemanantsoa.ma@gmail.com>
2026-05-09 16:46:07 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-function checkForm(){
-    let form = document.querySelectorAll(".formulaire");
-    form.forEach(element => {
-        element.addEventListener("submit", (e) => {
-            let complet = true;
-            let champRequis = element.querySelectorAll("[required]")
-
-            champRequis.forEach(elementChamp => {
-                let champ = elementChamp.value.trim();
-                if(!champ){
-                    elementChamp.style.border = "2px solid red";
-                    complet = false;
-                }
-                else{
-                    elementChamp.style.border = "";
-                }
-            });
-            if(!complet){
-                e.preventDefault();
-                alert("Veuillez remplir tous les champs requis. ");
-            }
-        });
-    });
-}
-
-function aimTestGame(){
-    checkForm();
-    
-}
 
 
 
